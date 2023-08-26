a7b89de5a8965d53a244045d04f44c5a0fc1c12e
CRISTHIAN <CRISTHIAN@DESKTOP-LS2ABPT>
2023-08-26 17:32:55 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+//Escribir un programa que convierta un valor
+//dado en grados Fahrenheit a grados Celsius
+
+Console.WriteLine("valor en Fahrenheit");
+double valor1= double.Parse(Console.ReadLine());
+
+double celsius = (valor1 - 32) * 5 / 9;
+
+Console.WriteLine(valor1 + " Fahrenheit equivalen a "+celsius+" grados celsius");
